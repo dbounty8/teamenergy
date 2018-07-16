3df1a149baa0aca4b192974504f2b317244964ae
Rob Lihou <rob@visiosound.co.uk>
2018-07-16 20:09:58 +0100

    Set port from env variable

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const PORT = process.env.PORT || 3000;
+
 // create express app
 const app = express();
 
@@ -67,7 +69,7 @@ app.get('/', (req, res) => {
 require('./app/routes/routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Express Server is listening on port 3000");
+app.listen(PORT, () => {
+    console.log(`Express Server is listening on port ${PORT}`);
 });
 
